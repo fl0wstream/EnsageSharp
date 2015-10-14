9dcb431d33b78fd669146edf5f565fdf7f220beb
Evervolv1337 <evervolspeed@gmail.com>
2015-10-14 20:54:18 +0500

    Added template for addons, EActivator testing :3

---
+++ EActivator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Activator
+{
+    internal class Program
+    {
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            Activator.Init();
+        }
+
+        #endregion
+    }
+}
---
+++ _template/Activator.cs
@@ -0,0 +1,43 @@
+﻿namespace Activator
+{
+    using System;
+    using System.Linq;
+    using System.Windows.Input;
+
+    using Ensage;
+    using Ensage.Common;
+    using Ensage.Common.Extensions;
+
+    using SharpDX;
+    using SharpDX.Direct3D9;
+
+    using unitDB = Ensage.Common.UnitDatabase;
+
+    internal class Activator
+    {
+        #region Static Fields
+
+        private static bool loaded;
+        private static Hero me;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public static void Init()
+        {
+            Game.OnUpdate += Game_OnUpdate;
+            loaded = false;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+        }
+
+        #endregion
+    }
+}
